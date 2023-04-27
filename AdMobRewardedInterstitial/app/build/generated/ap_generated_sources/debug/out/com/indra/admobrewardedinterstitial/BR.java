78f9b18a7dabf7439bd7ab15edefefa34a38f444
indrapatel8 <ivfire811@gmail.com>
2023-04-27 09:44:09 +0530

    Removed Rewarded

--- AdMobRewardedInterstitial/app/build/generated/ap_generated_sources/debug/out/com/indra/admobrewardedinterstitial/BR.java
+++
@@ -1,5 +0,0 @@
-package com.indra.admobrewardedinterstitial;
-
-public class BR {
-  public static final int _all = 0;
-}
--- AdMobRewardedInterstitial/app/build/generated/data_binding_trigger/debug/com/indra/admobrewardedinterstitial/DataBindingTriggerClass.java
+++
@@ -1,4 +0,0 @@
-package com.indra.admobrewardedinterstitial;
-
-@androidx.databinding.BindingBuildInfo
-public class DataBindingTriggerClass {}
